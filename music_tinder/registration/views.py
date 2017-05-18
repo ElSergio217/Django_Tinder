6ebe846c91f096d56ae76be1d069acace96e993e
ElSergio217 <sergioa.garcia217@gmail.com>
2017-05-17 22:29:53 -0400

    register completed

---
+++ music_tinder/registration/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def register(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('login')
+	else:
+		form = UserCreationForm()
+	context = dict(form=form)
+	return render(request, 'registration/register.html', context)
